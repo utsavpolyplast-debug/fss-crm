aa26c87105594b123e0a34338f63bc3d9c635aca
utsavpolyplast-debug <utsavpolyplast@gmail.com>
2026-04-28 14:32:07 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    domains: ['vkbidbwafrrlspjibyus.supabase.co']
-  }
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true }
 }
 module.exports = nextConfig
